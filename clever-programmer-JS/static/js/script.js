a6d071233ae2ee0388dbde7afd7b6506d441efca
shoneriki <ikirenohs@gmail.com>
2021-05-15 21:14:58 +0900

    result and message working

--- clever-programmer-JS/static/js/script.js
+++ clever-programmer-JS/static/js/script.js
@@ -37,13 +37,15 @@ catButton.addEventListener('click', function() {
 
 
 function rpsGame(yourChoice) {
-  console.log(yourChoice.id);
+  console.log(yourChoice);
   var humanChoice, botChoice;
   humanChoice = yourChoice.id
   botChoice = numberToChoice(randToRpsInt());
   console.log('Computer choice:', botChoice);
   results = decideWinner(humanChoice, botChoice);
-  // message = finalMessage(results) // {'message': "You Won", 'color': 'green' }
+  console.log(results)
+  message = finalMessage(results) // {'message': "You Won", 'color': 'green' }
+  console.log(message)
   // rpsFrontEnd(yourChoice.id, botChoice, message);
 }
 
@@ -55,15 +57,25 @@ function numberToChoice(number) {
   return ['rock', 'paper', 'scissors'][number];
 }
 
-function decideWinner(yourChoice, computerChoice) {
+function decideWinner(humanChoice, botChoice) {
   let rpsDatabase = {
     'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
     'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
     'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0},
   }
 
-  let yourScore = rpsDatabase[yourChoice][computerChoice];
-  let computerScore = rpsDatabase[computerChoice][yourChoice];
+  let yourScore = rpsDatabase[humanChoice][botChoice];
+  let computerScore = rpsDatabase[botChoice][humanChoice];
 
   return [yourScore, computerScore];
+}
+
+function finalMessage([yourScore, computerScore]) {
+  if (yourScore === 0) {
+    return {'message': 'You Lost!', 'color': 'red'}
+  } else if (yourScore === 0.5) {
+    return {'message': 'You Tied!', 'color': 'yellow'}
+  } else {
+    return {'message': 'You Won!', 'color': 'green'}
+  }
 }
