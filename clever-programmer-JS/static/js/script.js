bbbacc843a51a20264b59e201ca3dec5ad556861
shoneriki <ikirenohs@gmail.com>
2021-05-15 22:13:21 +0900

    took out unnecessary css code

--- clever-programmer-JS/static/js/script.js
+++ clever-programmer-JS/static/js/script.js
@@ -35,7 +35,6 @@ catButton.addEventListener('click', function() {
 
 // Challenge 3: Rock, Paper, Scissors
 
-
 function rpsGame(yourChoice) {
   console.log(yourChoice);
   var humanChoice, botChoice;
@@ -74,7 +73,7 @@ function finalMessage([yourScore, computerScore]) {
   if (yourScore === 0) {
     return {'message': 'You Lost!', 'color': 'red'}
   } else if (yourScore === 0.5) {
-    return {'message': 'You Tied!', 'color': 'yellow'}
+    return {'message': 'You Tied!', 'color': 'gold'}
   } else {
     return {'message': 'You Won!', 'color': 'green'}
   }
@@ -97,13 +96,12 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
   let messageDiv = document.createElement('div');
 
   humanDiv.innerHTML = `<img src='${imagesDatabase[humanImageChoice]}' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>`
-  // messageDiv.innerHTML = "<img src='" + imagesDatabase[finalMessage] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
-  messageDiv.innerHTML = "<h1 style = color: "
+  messageDiv.innerHTML = `<h1 style ='color: ${finalMessage['color']}; font-size: 60px; padding: 30px;'> ${finalMessage['message']} </h1>`
   botDiv.innerHTML = `<img src=' ${imagesDatabase[botImageChoice]} ' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>`
 
 
   document.getElementById('flex-box-rps-div').appendChild(humanDiv);
-  // document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+  document.getElementById('flex-box-rps-div').appendChild(messageDiv);
   document.getElementById('flex-box-rps-div').appendChild(botDiv);
 
 
